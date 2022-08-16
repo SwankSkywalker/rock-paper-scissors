26f81ef4b477a09196d17ebfe1035bccc3bb9dd7
Swank <willmadedesign@gmail.com>
2022-08-15 21:22:31 -0700

    Work on for looping round function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let myScore = 0
+let compScore = 0
+
 // Function for computer Selection of RPS
 
 function compuChoice() {
@@ -13,48 +16,60 @@ function compuChoice() {
     }
 
 }
-console.log(compuChoice());
+//console.log(compuChoice());
 
 //function that plays a single round of RPS
 
-
-function singleRound(playerSelection, compuChoice) {
+function playRound(playerSelection, compuChoice) {
    
-    if (compuChoice === 'Paper' && playerSelection === 'Rock') {
+    if (compuChoice === 'Paper' && playerSelection === 'rock') {
         return 'You lose! Paper beats Rock!';
     }
-    else if (compuChoice === 'Scissors' && playerSelection === 'Rock') {
+    else if (compuChoice === 'Scissors' && playerSelection === 'rock') {
         return 'You win! Rock beats Scissors!';
     }
-    else if (compuChoice === 'Rock' && playerSelection === 'Rock') {
+    else if (compuChoice === 'Rock' && playerSelection === 'rock') {
         return "It's a tie!";
     }
-    else if (compuChoice === 'Paper' && playerSelection === 'Paper') {
+    else if (compuChoice === 'Paper' && playerSelection === 'paper') {
         return "It's a tie!";
     }
-    else if (compuChoice === 'Paper' && playerSelection === 'Scissors') {
+    else if (compuChoice === 'Paper' && playerSelection === 'scissors') {
         return 'You Win! Scissors beats paper!';
     }
-    else if (compuChoice === 'Scissors' && playerSelection === 'Paper') {
+    else if (compuChoice === 'Scissors' && playerSelection === 'paper') {
         return 'You lose! Scissors beats paper!';
     }
-    else if (compuChoice === 'Scissors' && playerSelection === 'Scissors') {
+    else if (compuChoice === 'Scissors' && playerSelection === 'scissors') {
         return "It's a tie!";
     }
-    else if (compuChoice === 'Rock' && playerSelection === 'Scissors') {
+    else if (compuChoice === 'Rock' && playerSelection === 'scissors') {
         return 'You lose! Rock beats scissors!';
     }
-    else if (compuChoice === 'Rock' && playerSelection === 'Paper') {
+    else if (compuChoice === 'Rock' && playerSelection === 'paper') {
         return 'You win! Paper beats rock!';
     }
 }
-let playerSelection = prompt('Rock, Paper, Scissors');
+let playerSelection = prompt('Choose Rock, Paper or Scissors').toLowerCase();
 let compuSelection = compuChoice();
-console.log(singleRound(playerSelection, compuSelection));
+console.log(playRound(playerSelection, compuSelection));
 
 
 //function that makes games 5 rounds 
 
 function game() {
-    singleRound()
+    playRound(playerSelection, compuChoice);
+    for (let i = 0; i < 5; i++) {
+        if (playerSelection === 'rock' && compuChoice === 'Scissors') {
+            console.log('You win!' && ++myScore);
+        } else {
+            console.log('You lose!' &&  ++compScore);
+        }
+    }
 }
+console.log(game());
+
+
+
+
+
